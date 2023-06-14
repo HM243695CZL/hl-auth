842c6885b12b3df7e49bad849dd28ba89f4441d6
HM243695CZL <1224311695@qq.com>
2023-06-14 17:18:20 +0800

    '完善角色管理接口、添加自动填充的时间设置'

--- hl-admin/src/main/java/com/hl/admin/controller/UmsRoleController.java
+++ hl-admin/src/main/java/com/hl/admin/controller/UmsRoleController.java
@@ -12,8 +12,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 /**
  * <p>
  * 角色表 前端控制器
@@ -39,6 +37,30 @@ public class UmsRoleController {
         return CommonResult.success(CommonPage.restPage(roleList));
     }
 
+    // 新增
+    @LogAnnotation
+    @ApiOperation("新增角色")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult save(@RequestBody UmsRole umsRole){
+        return CommonResult.success(umsRoleService.save(umsRole));
+    }
+
+
+    // 更新
+    @LogAnnotation
+    @ApiOperation("更新角色")
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public CommonResult update(@RequestBody UmsRole umsRole){
+        return CommonResult.success(umsRoleService.updateById(umsRole));
+    }
+
+    // 删除
+    @LogAnnotation
+    @ApiOperation("删除角色")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public CommonResult delete(@PathVariable String id){
+        return CommonResult.success( umsRoleService.removeById(id));
+    }
 
     // 获取全部
     @LogAnnotation
@@ -46,6 +68,14 @@ public class UmsRoleController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public CommonResult list(){
         return CommonResult.success(umsRoleService.list());
+    }
+
+    // 查看
+    @LogAnnotation
+    @ApiOperation("查看角色")
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public CommonResult findOne(@PathVariable String id){
+        return CommonResult.success(umsRoleService.getById(id));
     }
 
 
