c6670e5dd33cb0ef6eb1dfbc083ac421539c1d08
HM243695CZL <1224311695@qq.com>
2023-06-15 10:32:13 +0800

    '添加jwt工具类'

---
+++ hl-common/src/main/java/com/hl/admin/utils/JwtHelper.java
@@ -0,0 +1,56 @@
+package com.hl.admin.utils;
+
+import io.jsonwebtoken.*;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+public class JwtHelper {
+
+    private static String tokenSignKey = "243695";
+
+    public static String createToken(String userId, String username) {
+        long tokenExpiration = 7 * 24 * 60 * 60 * 1000;
+        return Jwts.builder()
+                .setSubject("AUTH-USER")
+                .setExpiration(new Date(System.currentTimeMillis() + tokenExpiration))
+                .claim("userId", userId)
+                .claim("username", username)
+                .signWith(SignatureAlgorithm.HS512, tokenSignKey)
+                .compressWith(CompressionCodecs.GZIP)
+                .compact();
+    }
+
+    public static String getUserId(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return null;
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            return (String) claims.get("userId");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String getUsername(String token) {
+        try {
+            if (StringUtils.isEmpty(token)) return "";
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(tokenSignKey).parseClaimsJws(token);
+            Claims claims = claimsJws.getBody();
+            return (String) claims.get("username");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        String token = JwtHelper.createToken("8c2bf63982c58f7e43f14c6ddcb08581", "admin");
+        System.out.println(token);
+        System.out.println(JwtHelper.getUserId(token));
+        System.out.println(JwtHelper.getUsername(token));
+    }
+}
